83476655435a07a34d3a233bb8f05eaec048b8cc
Nguyễn Minh Duy <217141310+duyxyz@users.noreply.github.com>
2026-03-20 22:24:48 +0700

    sửa lỗi sidebar firefox

--- src/shared/browser-api.js
+++ src/shared/browser-api.js
@@ -5,14 +5,6 @@
   };
 
   const openPanelForCurrentTab = async () => {
-    const tab = await getActiveTab();
-    if (!tab) return false;
-
-    if (chrome.sidePanel && chrome.sidePanel.open) {
-      await chrome.sidePanel.open({ tabId: tab.id });
-      return true;
-    }
-
     if (typeof browser !== 'undefined' && browser.sidebarAction && browser.sidebarAction.open) {
       await browser.sidebarAction.open();
       return true;
@@ -20,6 +12,13 @@
 
     if (chrome.sidebarAction && chrome.sidebarAction.open) {
       await chrome.sidebarAction.open();
+      return true;
+    }
+
+    if (chrome.sidePanel && chrome.sidePanel.open) {
+      const tab = await getActiveTab();
+      if (!tab) return false;
+      await chrome.sidePanel.open({ tabId: tab.id });
       return true;
     }
 
